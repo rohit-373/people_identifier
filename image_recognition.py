32aab670115e2abcadb57bc6550aab9b8820399c
Rohit Elamurugan <48488024+Rohit-Elamurugan@users.noreply.github.com>
2021-03-03 11:17:40 +0530

    made it neater

--- image_recognition.py
+++ image_recognition.py
@@ -4,24 +4,24 @@ from pdf2image import convert_from_path
 
 temp = []
 
-images = convert_from_path('hallticket.pdf', poppler_path='Release-21.02.0/poppler-21.02.0/Library/bin')
+images = convert_from_path("person's_details/hallticket.pdf", poppler_path='Release-21.02.0/poppler-21.02.0/Library/bin')
 for i in range(len(images)):
-    images[i].save(f'page{i}.jpg', 'JPEG')
-    image = cv2.imread(f'page{i}.jpg', -1)
+    images[i].save(f'images/page{i}.jpg', 'JPEG')
+    image = cv2.imread(f'images/page{i}.jpg', -1)
     cascade = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(cascade)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         try:
-            cv2.imwrite('hallticket.jpg', image[y - 30:y + h + 30, x - 30:x + w + 30])
+            cv2.imwrite('images/hallticket.jpg', image[y - 30:y + h + 30, x - 30:x + w + 30])
         except:
             continue
 
-images = convert_from_path('interview.pdf', poppler_path='Release-21.02.0/poppler-21.02.0/Library/bin')
+images = convert_from_path("person's_details/interview.pdf", poppler_path='Release-21.02.0/poppler-21.02.0/Library/bin')
 for i in range(len(images)):
-    images[i].save(f'page{i}.jpg', 'JPEG')
-    image = cv2.imread(f'page{i}.jpg', -1)
+    images[i].save(f'images/page{i}.jpg', 'JPEG')
+    image = cv2.imread(f'images/page{i}.jpg', -1)
     cascade = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(cascade)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -30,12 +30,12 @@ for i in range(len(images)):
     for (x, y, w, h) in faces:
         j += 1
         try:
-            cv2.imwrite(f'face{j}.jpg', image[y - 30:y + h + 30, x - 30:x + w + 30])
-            temp.append(f'face{j}.jpg')
+            cv2.imwrite(f'images/face{j}.jpg', image[y - 30:y + h + 30, x - 30:x + w + 30])
+            temp.append(f'images/face{j}.jpg')
         except:
             continue
 
-hallticket_image = cv2.imread("hallticket.jpg")
+hallticket_image = cv2.imread("images/hallticket.jpg")
 hallticket_image = cv2.cvtColor(hallticket_image, cv2.COLOR_BGR2RGB)
 hallticket_image_encoding = fr.face_encodings(hallticket_image)
 
